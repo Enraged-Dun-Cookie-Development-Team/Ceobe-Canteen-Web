635c71401992d81c01ec4c241b233ba9acd6c6f8
Kaze <kaze.liu@qq.com>
2022-08-16 17:55:08 +0800

    :white_check_mark:完善安装方法的展示步骤

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,35 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {createVuetify} from 'vuetify'
 
 import '@/assets/style/main.scss'
 import '@/assets/icon/iconfont.css'
 import '@/assets/style/font.scss'
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'vuetify/dist/vuetify.css'
 
-createApp(App).use(store).use(router).mount('#app')
+import * as components from 'vuetify/lib/components/index'
+import * as directives from 'vuetify/lib/directives/index'
+import {aliases, mdi} from 'vuetify/lib/iconsets/mdi'
+import {fa} from 'vuetify/lib/iconsets/fa'
+
+import '@mdi/font/css/materialdesignicons.css'
+
+// https://materialdesignicons.com/ 图标字体
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+            fa
+        }
+    },
+});
+
+createApp(App).use(store).use(router).use(vuetify).mount('#app')
