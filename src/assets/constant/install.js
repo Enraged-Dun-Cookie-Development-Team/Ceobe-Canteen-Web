ffc75f80bb820c9d799ade9ca5ca85d631a87798
Kaze <kaze.liu@qq.com>
2022-08-17 15:47:21 +0800

    :zap: 更新浏览器安装手册

--- src/assets/constant/install.js
+++ src/assets/constant/install.js
@@ -41,6 +41,153 @@ const INSTALL_CHROME = {
         }
     ]
 }
-const PC_INSTALL_HELP_LIST = [INSTALL_CHROME];
+const INSTALL_EDGE = {
+    name: 'edge浏览器',
+    icon: 'edge.png',
+    help: [{
+        html: '下载插件端压缩包，并解压到任意文件夹',
+        image: '',
+        btn: [
+            {
+                text: '下载插件端压缩包',
+                link: 'https://github.com/Enraged-Dun-Cookie-Development-Team/Dun-Cookie-Vue/releases'
+            }, {
+                text: '点击这里跳转edge应用商店',
+                link: 'https://microsoftedge.microsoft.com/addons/detail/%E5%B0%8F%E5%88%BB%E9%A3%9F%E5%A0%82-%E6%98%8E%E6%97%A5%E6%96%B9%E8%88%9F%E8%B9%B2%E9%A5%BC%E5%99%A8-arknight/jimmfliacfpeabcifcghmdankmdnmfmn?hl=zh-CN+%E4%BD%9C%E8%80%85%EF%BC%9A%E6%B4%9B%E6%A2%A7%E8%97%A4+https%3A%2F%2Fwww.bilibili.com%2Fread%2Fcv13066589+%E5%87%BA%E5%A4%84%EF%BC%9Abilibili'
+            }]
+    },
+        {
+            html: `地址栏输入<span>edge://extensions/</span>，回车`,
+            flex: 'flex-between-center'
+        },
+        {
+            html: `点击左下角<span>打开开发者模式</span>!`,
+            flex: 'flex-around-center flex-column'
+        },
+        {
+            html: `点击已安装的扩展右边的第一个图标“加载解压缩的扩展”!`,
+            flex: 'flex-around-center flex-column'
+        },
+        {
+            html: `选择解压后的整个插件文件夹完成安装`,
+            tag: '可以通过地址栏输入`edge://settings/?search=后台`找到“关闭 Google Chrome 后继续运行后台应用”，选择运行后台扩展程序的使用',
+            flex: 'flex-around-center flex-column'
+        },
+        {
+            html: `还是不会？加群带你飞！`,
+            flex: 'flex-around-center flex-column',
+            btn: [{
+                text: '现在就加入蹲饼群！',
+                link: 'http://www.baidu.com'
+            }]
+        }
+    ]
+}
+const INSTALL_FIREFOX = {
+    name: 'Firefox浏览器',
+    icon: 'firefox.png',
+    help: [{
+        html: `地址栏输入<span>about:addons</span>，回车`,
+        flex: 'flex-between-center',
+        btn: [
+            {
+                text: '点击这里跳转Firefox应用商店',
+                link: 'https://addons.mozilla.org/zh-CN/firefox/addon/%E5%B0%8F%E5%88%BB%E9%A3%9F%E5%A0%82-%E6%98%8E%E6%97%A5%E6%96%B9%E8%88%9F%E8%B9%B2%E9%A5%BC%E5%99%A8-arknights-cookies/'
+            }]
+    },
+        {
+            html: `点击“接受风险并继续”!`,
+            flex: 'flex-around-center flex-column'
+        },
+        {
+            html: `在搜索框输入dom.events.asyncClipboard.clipboardItem`,
+            flex: 'flex-around-center flex-column'
+        },
+        {
+            html: `双击或者点击后面双箭头切换为“true”完成`,
+            tag: '安装完成后解压文件不要卸载了哦',
+            flex: 'flex-around-center flex-column'
+        },
+        {
+            html: `还是不会？加群带你飞！`,
+            flex: 'flex-around-center flex-column',
+            btn: [{
+                text: '现在就加入蹲饼群！',
+                link: 'http://www.baidu.com'
+            }]
+        }
+    ]
+}
+const INSTALL_QQ = {
+    name: 'QQ浏览器',
+    icon: 'qq.png',
+    help: [{
+        html: '下载插件端压缩包，并解压到任意文件夹',
+        image: '',
+        btn: [
+            {
+                text: '下载插件端压缩包',
+                link: 'https://github.com/Enraged-Dun-Cookie-Development-Team/Dun-Cookie-Vue/releases'
+            }]
+    },
+        {
+            html: `地址栏输入<span>qqbrowser://extensions/manage</span>，回车`,
+            flex: 'flex-between-center'
+        },
+        {
+            html: `点击右上角，打开开发者模式，并点击“加载已解压的扩展程序”`,
+            flex: 'flex-around-center flex-column'
+        },
+        {
+            html: `添加扩展程序，选择解压后的整个插件文件夹完成安装`,
+            tag: '安装完成后解压文件不要卸载了哦',
+            flex: 'flex-around-center flex-column'
+        },
+        {
+            html: `还是不会？加群带你飞！`,
+            flex: 'flex-around-center flex-column',
+            btn: [{
+                text: '现在就加入蹲饼群！',
+                link: 'http://www.baidu.com'
+            }]
+        }
+    ]
+}
+const INSTALL_360 = {
+    name: '360浏览器',
+    icon: '360.png',
+    help: [{
+        html: '下载插件端压缩包，并解压到任意文件夹',
+        image: '',
+        btn: [
+            {
+                text: '下载插件端压缩包',
+                link: 'https://github.com/Enraged-Dun-Cookie-Development-Team/Dun-Cookie-Vue/releases'
+            }]
+    },
+        {
+            html: `点击地址栏旁边扩展中心图标，点击已安装的扩展`,
+            flex: 'flex-between-center'
+        },
+        {
+            html: `点击右上角，打开开发者模式，并点击左边的“加载已解压的扩展程序”`,
+            flex: 'flex-around-center flex-column'
+        },
+        {
+            html: `添加扩展程序，选择解压后的整个插件文件夹完成安装`,
+            tag: '安装完成后解压文件不要卸载了哦',
+            flex: 'flex-around-center flex-column'
+        },
+        {
+            html: `还是不会？加群带你飞！`,
+            flex: 'flex-around-center flex-column',
+            btn: [{
+                text: '现在就加入蹲饼群！',
+                link: 'http://www.baidu.com'
+            }]
+        }
+    ]
+}
+const PC_INSTALL_HELP_LIST = [INSTALL_CHROME, INSTALL_EDGE, INSTALL_FIREFOX, INSTALL_QQ, INSTALL_360];
 
 export {PC_INSTALL_HELP_LIST}
