17f1824ef65b5ee4f60008b19eb5b646840ad6b6
phidiaLam <2957035701@qq.com>
2022-10-06 19:59:36 +0800

    🔧 修改配置，修复打包空白问题

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: './',
+  outputDir:'dist',
+  assetsDir: 'assets'
 })
