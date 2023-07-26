b272882e8445d52983a8b9166e9e143999e987d9
phidiaLam <2957035701@qq.com>
2023-07-26 18:52:13 +0800

    🩹 限制打包文件大小

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,13 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: './',
   outputDir:'dist',
-  assetsDir: 'assets'
+  assetsDir: 'assets',
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config["performance"] = {
+        "maxEntrypointSize": 10000000,
+        "maxAssetSize": 30000000
+      }
+    }
+  }
 })
